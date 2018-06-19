1af5c30d1eba065ed7bafe9c3c9ac528f6d9c24c
Stefan <Stefan@Atzi-XPS>
2018-06-19 14:32:19 +0200

    delete the other project

--- Game/Include/AnimationListener.h
+++
@@ -1,9 +0,0 @@
-namespace arcomage {
-	class AnimationListener {
-	public:
-		virtual void animationStarted() = 0;
-		virtual void animationFinished() = 0;
-		virtual Gamestate getState() = 0;
-		virtual void notifyAnimationIdle() = 0;
-	};
-}
